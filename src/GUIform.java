f06f51d79e8e663832148e2edfd8f34a5ec5d655
mtdelbuono <milan.del.buono@gmail.com>
2024-06-05 23:43:40 -0700

    New Color GUI, needs functionality.

--- src/GUIform.java
+++ src/GUIform.java
@@ -128,60 +128,164 @@ public class GUIform implements ActionListener {
         }
     }
 
-    protected void recolor(){// mega debug this shit..
-        //get the content from the three panels. (later)
-        JInternalFrame recolorFrame = new JInternalFrame("Manual Image Recolor", true,true,true,true);
-        recolorFrame.setLayout(new BorderLayout());
-        recolorFrame.setSize(2000, 800);
-        frame.add(recolorFrame);
+    protected void recolor(){
+       /*
+       @Todo
+        1. Set frame to exit on save, cancel, close.
+        2. Figure out sliderlisteners and make the RescaleOp for the sliders. (ChangeListener + ImageOp)
+        3. Figure out how to populate said images. For next commit just add some random 3.
+        */
+        JInternalFrame frmManualRecoloring = new JInternalFrame("Manual Image Recolor", true, true,true,true);
+       // frmManualRecoloring.setBounds(100, 100, 580, 460);
+        frame.getContentPane().add(frmManualRecoloring);
+        frmManualRecoloring.setTitle("Manual Recoloring");
+        frmManualRecoloring.setDefaultCloseOperation(JInternalFrame.EXIT_ON_CLOSE);
+        frmManualRecoloring.getContentPane().setLayout(null);
 
-        BoundedRangeModel red = new DefaultBoundedRangeModel(0, 1, -255, 255);
-        BoundedRangeModel green = new DefaultBoundedRangeModel(0, 1, -255, 255);
-        BoundedRangeModel blue = new DefaultBoundedRangeModel(0, 1, -255, 255);
-        // these to be replaced by images (getter from the JTabbedPane)
-        JPanel recolorImages = new JPanel();
-        recolorImages.setLayout(new BoxLayout(recolorImages, BoxLayout.X_AXIS));
+        JButton save = new JButton("Save");
+        save.setBounds(478, 397, 85, 21);
+        frmManualRecoloring.getContentPane().add(save);
 
-        //fake images
-        JPanel panel1, panel2, panel3;
-        panel1 = new JPanel();
-        panel1.setBackground(Color.BLUE);
-        panel1.setPreferredSize(new Dimension(400,500));
-        panel2 = new JPanel();
-        panel2.setPreferredSize(new Dimension(400,500));
-        panel2.setBackground(Color.RED);
-        panel3 = new JPanel();
-        panel3.setBackground(Color.GREEN);
-        panel3.setPreferredSize(new Dimension(400,500));
+        JButton cancel = new JButton("Cancel");
+        cancel.setBounds(387, 397, 85, 21);
+        frmManualRecoloring.getContentPane().add(cancel);
 
-        recolorImages.add(panel1);
-        recolorImages.add(panel2);
-        recolorImages.add(panel3);
+        JPanel imgpanel1 = new JPanel();
+        imgpanel1.setBounds(10, 25, 160, 252);
+        frmManualRecoloring.getContentPane().add(imgpanel1);
+        imgpanel1.setLayout(null);
 
-        sliderListener = new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                //nothing yet.
-            }
-        };
+        JPanel redref1 = new JPanel();
+        redref1.setBackground(new Color(255, 0, 0));
+        redref1.setBounds(0, 242, 57, 10);
+        imgpanel1.add(redref1);
 
-        red.addChangeListener(sliderListener);
-        green.addChangeListener(sliderListener);
-        blue.addChangeListener(sliderListener);
+        JPanel greenref1 = new JPanel();
+        greenref1.setBackground(new Color(0, 255, 0));
+        greenref1.setBounds(54, 242, 57, 10);
+        imgpanel1.add(greenref1);
 
-        JSlider redSlider = new JSlider(red);
-        JSlider greenSlider = new JSlider(green);
-        JSlider blueSlider = new JSlider(blue);
+        JPanel photoholder1 = new JPanel();
+        photoholder1.setBackground(new Color(192, 192, 192));
+        photoholder1.setBounds(0, 10, 160, 222);
+        imgpanel1.add(photoholder1);
 
-        JPanel sliders = new JPanel();
-        sliders.add(redSlider);
-        sliders.add(greenSlider);
-        sliders.add(blueSlider);
+        JPanel blueref1 = new JPanel();
+        blueref1.setBackground(new Color(0, 128, 255));
+        blueref1.setBounds(109, 242, 51, 10);
+        imgpanel1.add(blueref1);
 
+        JPanel imgpanel2 = new JPanel();
+        imgpanel2.setBounds(180, 25, 160, 252);
+        frmManualRecoloring.getContentPane().add(imgpanel2);
+        imgpanel2.setLayout(null);
 
-        recolorFrame.getContentPane().add(recolorImages);
-        recolorFrame.getContentPane().add(sliders, BorderLayout.SOUTH);
-        recolorFrame.setVisible(true);
+        JPanel greenref2 = new JPanel();
+        greenref2.setBackground(Color.GREEN);
+        greenref2.setBounds(54, 242, 57, 10);
+        imgpanel2.add(greenref2);
+
+        JPanel redref2 = new JPanel();
+        redref2.setBackground(Color.RED);
+        redref2.setBounds(0, 242, 57, 10);
+        imgpanel2.add(redref2);
+
+        JPanel photoholder2 = new JPanel();
+        photoholder2.setBackground(new Color(192, 192, 192));
+        photoholder2.setBounds(0, 10, 160, 222);
+        imgpanel2.add(photoholder2);
+
+        JPanel blueref2 = new JPanel();
+        blueref2.setBackground(new Color(0, 128, 255));
+        blueref2.setBounds(109, 242, 51, 10);
+        imgpanel2.add(blueref2);
+
+        JPanel imgpanel3 = new JPanel();
+        imgpanel3.setBounds(350, 25, 160, 252);
+        frmManualRecoloring.getContentPane().add(imgpanel3);
+        imgpanel3.setLayout(null);
+
+        JPanel photoholder3 = new JPanel();
+        photoholder3.setBackground(new Color(192, 192, 192));
+        photoholder3.setBounds(0, 10, 160, 222);
+        imgpanel3.add(photoholder3);
+
+        JPanel redref3 = new JPanel();
+        redref3.setBackground(Color.RED);
+        redref3.setBounds(0, 242, 57, 10);
+        imgpanel3.add(redref3);
+
+        JPanel greenref3 = new JPanel();
+        greenref3.setBackground(Color.GREEN);
+        greenref3.setBounds(54, 242, 57, 10);
+        imgpanel3.add(greenref3);
+
+        JPanel blueref3 = new JPanel();
+        blueref3.setBackground(new Color(0, 128, 255));
+        blueref3.setBounds(109, 242, 51, 10);
+        imgpanel3.add(blueref3);
+
+        JSlider red1 = new JSlider(-255,255);
+        red1.setBounds(40, 287, 130, 26);
+        frmManualRecoloring.getContentPane().add(red1);
+
+        JSlider green1 = new JSlider(-255,255);
+        green1.setBounds(40, 323, 130, 26);
+        frmManualRecoloring.getContentPane().add(green1);
+
+        JSlider blue1 = new JSlider(-255,255);
+        blue1.setBounds(40, 362, 130, 26);
+        frmManualRecoloring.getContentPane().add(blue1);
+
+        JSlider blue2 = new JSlider(-255,255);
+        blue2.setBounds(180, 362, 160, 26);
+        frmManualRecoloring.getContentPane().add(blue2);
+
+        JSlider green2 = new JSlider(-255,255);
+        green2.setBounds(180, 323, 160, 26);
+        frmManualRecoloring.getContentPane().add(green2);
+
+        JSlider red2 = new JSlider(-255,255);
+        red2.setBounds(180, 287, 160, 26);
+        frmManualRecoloring.getContentPane().add(red2);
+
+        JSlider blue3 = new JSlider(-255,255);
+        blue3.setBounds(350, 361, 160, 26);
+        frmManualRecoloring.getContentPane().add(blue3);
+
+        JSlider green3 = new JSlider(-255,255);
+        green3.setBounds(350, 322, 160, 26);
+        frmManualRecoloring.getContentPane().add(green3);
+
+        JSlider red3 = new JSlider(-255,255);
+        red3.setBounds(350, 286, 160, 26);
+        frmManualRecoloring.getContentPane().add(red3);
+
+        JLabel redLabel1 = new JLabel("Red");
+        redLabel1.setBounds(10, 287, 45, 13);
+        frmManualRecoloring.getContentPane().add(redLabel1);
+
+        JLabel beforePic = new JLabel("Before");
+        beforePic.setBounds(10, 10, 45, 13);
+        frmManualRecoloring.getContentPane().add(beforePic);
+
+        JLabel after48Pic = new JLabel("48 Hr");
+        after48Pic.setBounds(180, 10, 45, 13);
+        frmManualRecoloring.getContentPane().add(after48Pic);
+
+        JLabel after72Pic = new JLabel("72 Hr");
+        after72Pic.setBounds(350, 10, 45, 13);
+        frmManualRecoloring.getContentPane().add(after72Pic);
+
+        JLabel lblGreen = new JLabel("Green");
+        lblGreen.setBounds(10, 323, 45, 13);
+        frmManualRecoloring.getContentPane().add(lblGreen);
+
+        JLabel lblBlue = new JLabel("Blue");
+        lblBlue.setBounds(10, 359, 45, 13);
+        frmManualRecoloring.getContentPane().add(lblBlue);
+
+        frmManualRecoloring.setVisible(true);
     }
 
 }
